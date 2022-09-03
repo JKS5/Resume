e1b451414052504734c981bd5e7cb45c63364d86
JKS5 <jokekhanson@gmail.com>
2022-09-03 20:20:57 +0900

    [React][feature][확인차]push

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -90,9 +90,7 @@ module.exports = {
       "3xl": "1936px",
       // => @media (min-width: 1936px) { ... }
     },
-    clipPath: {
-      mypolygon: "polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 1rem))",
-    },
+ 
   },
   plugins: [require("tailwindcss-animation-delay", "tailwind-clip-path")],
 };
